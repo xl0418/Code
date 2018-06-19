76a321183777261c7059e957260d9479f0d3e314
Liang Xu <xl0418@gmail.com>
2018-06-19 14:33:11 +0200

    Animating trait evolution along a given tree
    Animating evolution in a given tree;
    Single tree simulate and plot for project 2 have been added.

--- Pro2/Python_p2/DV_model_sim_along_phy.py
+++ Pro2/Python_p2/DV_model_sim_along_phy.py
@@ -1,7 +1,9 @@
+from matplotlib.pylab import *
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
+import matplotlib.animation as animation
+
 
 # timelist=pd.read_csv('C:\\Liang\\Googlebox\\Python\\Project2\\R-tree_sim\\timelist.csv', sep='',header=None)
 
@@ -35,7 +37,7 @@ timeend = np.delete(timeend,0)
 timeend = [int(x)-1 for x in timeend]
 
 theta = 0   # optimum of natural selection
-gamma = 0.01 # intensity of natural selection
+gamma = 0.001 # intensity of natural selection
 gamma1 = gamma #Growth rate's gamma
 gamma_K2 = gamma # Carrying capacity's gamma; It can be different from growth rate's gamma.
 h=1 # Inheritence
@@ -100,17 +102,17 @@ total_species = len(speciate_time)
 # Initialize trait evolution and population evolution matrices
 trait_RI_dr = np.zeros((evo_time + 1, total_species))
 population_RI_dr = np.zeros((evo_time + 1, total_species))
-trait_RI_dk = np.zeros((evo_time + 1, total_species))
-population_RI_dk = np.zeros((evo_time + 1, total_species))
+# trait_RI_dk = np.zeros((evo_time + 1, total_species))
+# population_RI_dk = np.zeros((evo_time + 1, total_species))
 
 #  initialize condition for species trait and population
-mu_trait, sigma_trait = 0, 10  # mean and standard deviation
-trait_RI_dr[0, (0,1)] = np.random.normal(mu_trait, sigma_trait, 2)
-trait_RI_dk[0] = trait_RI_dr[0]
+# mu_trait, sigma_trait = 0, 10  # mean and standard deviation
+trait_RI_dr[0, (0,1)] = 0    #np.random.normal(mu_trait, sigma_trait, 2)
+# trait_RI_dk[0] = trait_RI_dr[0]
 print(trait_RI_dr[0])
 mu_pop, sigma_pop = 500, 10  # mean and standard deviation
 population_RI_dr[0, (0,1)] = np.random.normal(mu_pop, sigma_pop, 2)
-population_RI_dk[0] = population_RI_dr[0]
+# population_RI_dk[0] = population_RI_dr[0]
 print(population_RI_dr[0])
 V = np.zeros((evo_time + 1, total_species))
 V.fill(1 / total_species)
@@ -174,23 +176,17 @@ for i in range(evo_time):
                                                                                        parent_index[spe_event_index-1]-1]
         population_RI_dr[i + 1, parent_index[spe_event_index-1]-1] = 1 / 2 * population_RI_dr[i + 1,
                                                                                         parent_index[spe_event_index-1]-1]
-        trait_RI_dk[i + 1, daughter_index[spe_event_index-1]-1] = trait_RI_dk[i + 1,
-                                                        parent_index[spe_event_index-1]-1] + np.random.normal(0, 0.01, 1)
-        population_RI_dk[i + 1, daughter_index[spe_event_index-1]-1] = 1 / 2 * \
-                                                                       population_RI_dk[i + 1, parent_index[spe_event_index-1]-1]
-        population_RI_dk[i + 1, parent_index[spe_event_index-1]-1] = 1 / 2 * \
-                                                                     population_RI_dk[i + 1, parent_index[spe_event_index-1]-1]
+        V[i+1,daughter_index[spe_event_index-1]-1] = 1/2 * V[i+1,parent_index[spe_event_index-1]-1]
+        V[i+1,parent_index[spe_event_index-1]-1] = 1/2 * V[i+1,parent_index[spe_event_index-1]-1]
 
     if (i + 1) in extinct_time:
         extinct_species = int(np.where(extinct_time == (i+1))[0])
         trait_RI_dr[i+1, extinct_species] = None
         population_RI_dr[i+1, extinct_species] = 0
-        trait_RI_dk[i + 1, extinct_species] = None
-        population_RI_dk[i + 1, extinct_species] = 0
+
 
 
 trait_RI_dr[np.where(trait_RI_dr == 0)[0],np.where(trait_RI_dr == 0)[1]] = None
-trait_RI_dk[np.where(trait_RI_dk == 0)[0],np.where(trait_RI_dk == 0)[1]] = None
 
 
 
@@ -199,49 +195,47 @@ num_plots = total_species
 
 # Have a look at the colormaps here and decide which one you'd like:
 # http://matplotlib.org/1.2.1/examples/pylab_examples/show_colormaps.html
-from cycler import cycler
-colors = [plt.cm.nipy_spectral(i) for i in np.linspace(0, 1, total_species)]
-
-colormap = plt.cm.gist_ncar
-plt.gca().set_prop_cycle(cycler('color', colors))
+# from cycler import cycler
+# colors = [plt.cm.nipy_spectral(i) for i in np.linspace(0, 1, total_species)]
+#
+# colormap = plt.cm.gist_ncar
+# plt.gca().set_prop_cycle(cycler('color', colors))
 
 # Plot several different functions...
 
 
 trait_dr_tips = trait_RI_dr[evo_time,:][~np.isnan(trait_RI_dr[evo_time,:])]
-trait_dk_tips = trait_RI_dk[evo_time,:][~np.isnan(trait_RI_dk[evo_time,:])]
 population_RI_dr[population_RI_dr == 0] = None
-population_RI_dk[population_RI_dk == 0] = None
 
 
 
 x = np.arange(evo_time+1)
 labels = []
-plt.subplot(2, 4, 1)
+plt.subplot(2, 2, 1)
 for i in range(1, num_plots + 1):
     plt.plot(x, trait_RI_dr[:,i-1])
 
-plt.subplot(2, 4, 2)
+plt.subplot(2, 2, 2)
 for i in range(1, num_plots + 1):
     plt.plot(x, population_RI_dr[:,i-1])
-plt.subplot(2, 4, 3)
+plt.subplot(2, 2, 3)
 sns.distplot(trait_dr_tips, hist=False, rug=True)
 
-plt.subplot(2, 4, 4)
+plt.subplot(2, 2, 4)
 sns.distplot(population_RI_dr[evo_time,:], hist=False, rug=True)
-
-plt.subplot(2, 4, 5)
-for i in range(1, num_plots + 1):
-    plt.plot(x, trait_RI_dk[:,i-1],linewidth=0.1)
-
-plt.subplot(2, 4, 6)
-for i in range(1, num_plots + 1):
-    plt.plot(x, population_RI_dk[:,i-1])
-plt.subplot(2, 4, 7)
-sns.distplot(trait_dk_tips, hist=False, rug=True)
-
-plt.subplot(2, 4, 8)
-sns.distplot(population_RI_dk[evo_time,:], hist=False, rug=True)
+#
+# plt.subplot(2, 4, 5)
+# for i in range(1, num_plots + 1):
+#     plt.plot(x, trait_RI_dk[:,i-1],linewidth=0.1)
+#
+# plt.subplot(2, 4, 6)
+# for i in range(1, num_plots + 1):
+#     plt.plot(x, population_RI_dk[:,i-1])
+# plt.subplot(2, 4, 7)
+# sns.distplot(trait_dk_tips, hist=False, rug=True)
+#
+# plt.subplot(2, 4, 8)
+# sns.distplot(population_RI_dk[evo_time,:], hist=False, rug=True)
 
 # I'm basically just demonstrating several different legend options here...
 # plt.legend(labels, ncol=4, loc='upper center',
@@ -262,3 +256,189 @@ print(trait_RI_dr[evo_time,:])
 # plt.hist(x, bins, alpha=0.5, label= 'BH')
 # plt.hist(y, bins, alpha=0.5, label= 'RI')
 # plt.show()
+
+
+
+# Animating trait evolution along a tree
+
+
+
+ext_times_RI = []
+
+ext_spec_index_RI = np.where(population_RI_dr[evo_time,] == 0)[0]
+# ext_spec_index_RI_rs = np.where(population_RI_rs[evo_time,] == 0)[0]
+
+
+for j in ext_spec_index_RI:
+    ext_time = np.where(population_RI_dr[:,j] == 0)
+    ext_times_RI.append(ext_time[0][0])
+
+#
+# for j in ext_spec_index_RI_rs:
+#     ext_time_rs = np.where(population_RI_rs[:,j] == 0)
+#     ext_times_RI_rs.append(ext_time_rs[0][0])
+
+x = np.array(range(evo_time))
+RI_traits = []
+RI_sizes = []
+# RI_traits_rs = []
+# RI_sizes_rs = []
+for i in np.arange(total_species):
+    RI_trait = trait_RI_dr[x,i]
+    RI_size = population_RI_dr[x,i]
+    RI_traits.append(RI_trait)
+    RI_sizes.append(RI_size)
+
+    # RI_trait_rs = trait_RI_rs[x,i]
+    # RI_size_rs = population_RI_rs[x,i]
+    # RI_traits_rs.append(RI_trait_rs)
+    # RI_sizes_rs.append(RI_size_rs)
+
+
+f0 = figure(num = 0, figsize = (12, 8))#, dpi = 100)
+f0.suptitle("Trait Evolution", fontsize=12)
+ax01 = subplot2grid((1, 1), (0, 0))
+# ax02 = subplot2grid((2, 1), (1, 0))
+
+
+i = theta
+ax01.axhline(y = i, color='k', linestyle='--',alpha=0.7)
+# ax02.axhline(y = i, color='k', linestyle='--',alpha=0.7)
+
+
+
+
+ax01.set_title('RI-sd')
+# ax02.set_title('RI-rs')
+
+
+trait_max = np.nanmax(trait_RI_dr)
+trait_min = np.nanmin(trait_RI_dr)
+
+
+# set y-limits
+ax01.set_ylim(trait_min-10,trait_max+10)
+# ax02.set_ylim(trait_min-10,trait_max+10)
+
+# ax03.set_ylim(-0,5)
+# ax04.set_ylim(-10,10)
+
+# sex x-limits
+ax01.set_xlim(0, evo_time)
+# ax02.set_xlim(0, evo_time)
+
+# ax03.set_xlim(0,5.0)
+# ax04.set_xlim(0,5.0)
+
+# Turn on grids
+ax01.grid(True)
+# ax02.grid(True)
+
+
+#
+ax01.set_xlabel("")
+ax01.set_ylabel("Trait Value")
+ax01.set_xlabel("Generation")
+ax01.set_ylabel("Trait Value")
+
+popu_RI_spec_texts= []
+
+
+text_y1 = np.linspace(0.9, 0.9 - (total_species - 1) * 0.05, total_species)
+
+for i in np.arange(total_species):
+    # popu_rs_spec_text1 = ax02.text(1,text_y2[i], '', transform = ax02.transAxes)
+    popu_RI_spec_text1 = ax01.text(1,text_y1[i],'', transform = ax01.transAxes)
+    # popu_rs_spec_texts.append(popu_rs_spec_text1)
+    popu_RI_spec_texts.append(popu_RI_spec_text1)
+
+RI_lines = []
+RI_scatters = []
+
+
+
+for i in  np.arange(total_species):
+    RI_line, = ax01.plot([],[], 'b-')
+    # rs_line, = ax02.plot([],[], 'b-')
+
+    RI_scatter = ax01.scatter([], [], s=0, c='r', alpha=0.3)
+    # rs_scatter = ax02.scatter([], [], s=0, c='r', alpha=0.3)
+
+    RI_lines.append(RI_line)
+    RI_scatters.append(RI_scatter)
+    # rs_lines.append(rs_line)
+    # rs_scatters.append(rs_scatter)
+
+# specieation and extinction table
+exist_index = np.where(extinct_time == -1)[0]
+extinct_only = np.delete(extinct_time,exist_index)
+
+events_time = np.concatenate((speciate_time,extinct_only))
+
+def animate(i):
+    i = (i+1)%(len(x)+1)
+    time_text.set_text(time_template % (i))
+    RI_datas = []
+    for j in np.arange(total_species):
+        # rs_data = np.hstack((x[i], RI_traits_rs[j][i]))
+        RI_data = np.hstack((x[i], RI_traits[j][i]))
+        # rs_datas.append(rs_data)
+        RI_datas.append(RI_data)
+
+    # for j in np.arange(total_species):
+    #     rs_lines[j].set_data(x[0:i], RI_traits_rs[j][0:i])
+        RI_lines[j].set_data(x[0:i], RI_traits[j][0:i])
+        # rs_scatters[j].set_offsets(rs_datas[j])
+        RI_scatters[j].set_offsets(RI_datas[j])
+        # rs_scatters[j].set_sizes([RI_sizes_rs[j][i]])
+        RI_scatters[j].set_sizes([RI_sizes[j][i]])
+        # BH_lines[j].set_label("spec %d" % j)
+        # Extinct species being labeled by dashed lines
+        # if (population_BH[i, j] == 0):
+        #     BH_lines[j].set_dashes([2, 2, 2, 2])
+        #     BH_lines[j].set_color("red")
+        # if (population_RI[i, j] == 0):
+        #     RI_lines[j].set_dashes([2, 2, 2, 2])
+        #     RI_lines[j].set_color("red")
+
+        # Animating labels
+        popu_RI_spec_texts[j].set_text('POS %d = %.1f' % (j+1, population_RI_dr[i,j]))
+        # popu_rs_spec_texts[j].set_text('POS %d = %.1f' % (j+1, population_RI_rs[i,j]))
+        if (i < speciate_time[j]):
+            # rs_lines[2].set_data([], [])
+            RI_lines[j].set_data([], [])
+            # rs_lines[1].set_data(x[0:i], RI_traits_rs[1][0:i])
+            # RI_lines[1].set_data(x[0:i], RI_traits[1][0:i])
+        elif (i >= speciate_time[j] and i < extinct_time[j]):
+            # rs_lines[2].set_data(x[1000:i], RI_traits_rs[2][1000:i])
+            RI_lines[j].set_data(x[speciate_time[j]:i], RI_traits[j][speciate_time[j]:i])
+            # rs_lines[1].set_data(x[0:i], RI_traits_rs[1][0:i])
+            # RI_lines[1].set_data(x[0:i], RI_traits[1][0:i])
+        else:
+            # rs_lines[2].set_data(x[1000:i], RI_traits_rs[2][1000:i])
+            RI_lines[j].set_data(x[speciate_time[j]:extinct_time[j]],
+                                    RI_traits[j][speciate_time[j]:extinct_time[j]])
+            # rs_lines[1].set_data(x[0:2000], RI_traits_rs[1][0:2000])
+            # RI_lines[1].set_data(x[0:2000], RI_traits[1][0:2000])
+
+        if (j in ext_spec_index_RI):
+            end_time_RI = ext_times_RI[np.where(j == ext_spec_index_RI)[0][0]]
+            if (i >= end_time_RI):
+                RI_lines[j].set_data(x[0:end_time_RI], RI_traits[j][0:end_time_RI])
+
+    # RI_lines[1].set_color("green")
+    # RI_lines[2].set_color("green")
+
+    return  RI_lines, RI_scatters
+
+
+##
+time_template = 'Time = %d G'    # prints running simulation time
+time_text = ax01.text(0.05, 1, '', transform=ax01.transAxes)
+#
+ani = animation.FuncAnimation(f0, animate, interval= 1, frames= evo_time, repeat=False, blit=False) #, init_func=init)
+plt.show()
+#
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+ani.save('C:\\Liang\\DVmodel_alongtree.mp4', writer=writer)
---
+++ Pro2/R_p2/Plottree_single_Pro1.R
@@ -0,0 +1,100 @@
+library(DDD)
+library(MASS)
+library(rgl)
+library(stringr)
+library(phytools)
+library(plotrix)
+source(paste0('C:/Liang/Googlebox/Research/Project1/R_pro1/Final/L2phylo_loc.R'))
+source(paste0('C:/Liang/Googlebox/Research/Project1/R_pro1/Final/splitAt.R'))
+
+#cycle = T: open tree label that is tree tips will be labeled by cycles; cycle = F: close
+#ls = 1: legend, crown age and titles will be drawn.
+
+plottree <- function(age = 15, dropextinct = T,file = filename, ls = 1,cycle = T){
+      maps_loc_new = list()
+      maps_loc_color = list()
+      
+     load(file = file)
+      L = result$L
+      track_loc = result$track_loc
+      phy = L2phylo_loc(L,loc = 1,dropextinct = dropextinct)
+      lin = as.numeric(phy$tip.label)
+      phy$tip.label = L[lin,5]
+     
+      
+      if(dropextinct == F) {break_point = round(unname(as.numeric(result$brts[,1])),digits = 6)}
+      #branching time as the split positions
+      
+      
+      if(dropextinct == T){brts=unname(sort(branching.times(phy),decreasing = T))
+      break_point = round(age-brts,digits = 6)}
+      
+      lin_sur_position=which(L[,4]==-1)
+      lin_sur_index = which(lin %in% lin_sur_position)
+      j=0
+      #add the present time to the survived lineages
+      for(k in lin[lin_sur_index]){
+        track_loc[[k]] = c(track_loc[[k]],age)
+      }
+      
+      #split the list consisting of vectors that contain the states of species and duration time. 
+      
+      for(i in lin){
+        j=j+1
+        names_new=names(track_loc[[i]] )[-length(names(track_loc[[i]] ))]
+        maps_loc_new[[j]] = diff( track_loc[[i]] )
+        names(maps_loc_new[[j]]) = names_new
+        names(maps_loc_new[[j]])[length(names(maps_loc_new[[j]]))] = c(paste0(names(maps_loc_new[[j]])[length(names(maps_loc_new[[j]]))],".",i))
+        
+        pos = which(round( track_loc[[i]],digits = 6) %in% break_point)[-1]
+        maps_loc_color = c(maps_loc_color,splitAt(maps_loc_new[[j]],pos))
+      }
+      map_loc_real_color = unique(maps_loc_color)
+     
+      for(c in 1:length(map_loc_real_color)){
+        names(map_loc_real_color[[c]])[length(map_loc_real_color[[c]])] = c(paste(trunc( as.numeric(names(map_loc_real_color[[c]])[length(map_loc_real_color[[c]])]))))
+      }
+      cols<-setNames(c("blue","red","green"),c("1","2","3"))
+      phy$maps = map_loc_real_color
+      
+      #add legend for the colors
+      if(ls == 1)  {
+        plotSimmap(phy,cols,offset = 10,ylim=c(-4,Ntip(phy)),fsize = 1,mar = c(4,1,1,1),split.vertical=TRUE)
+        cols_legend = cols 
+      names(cols_legend) = c("   Endemic","   Endemic","   Widespread")
+      add.simmap.legend(colors=cols_legend,prompt=FALSE,x=0,y=-2,vertical=FALSE)
+      axis(1)
+      #add the title
+      title(xlab="Time from the root")
+      }
+      #add the x-axis
+      else plotSimmap(phy,cols,offset = 10,fsize = 1,split.vertical=TRUE)
+      
+      #handy function: Click where you want to draw the legend
+      # add.simmap.legend(colors=cols,prompt=TRUE, vertical=FALSE)
+      
+      #loc info on the tips
+      if(dropextinct == T & cycle == T){
+        phy_loc = result$loctable[lin,]
+        
+        obj<-get("last_plot.phylo",envir=.PlotPhyloEnv)
+        x.tip<-obj$xx[1:obj$Ntip]
+        y.tip<-obj$yy[1:obj$Ntip]
+        
+        x_cor_vector1 = x.tip+1*strwidth("W")
+        for(i in 1:length(x.tip)){
+          x_cor = x_cor_vector1[i]
+          y_cor = y.tip[i]
+          if(phy_loc[,1][i] == 1) draw.circle(x_cor,y_cor,nv=1000,radius=0.08,col="purple")
+          else       draw.circle(x_cor,y_cor,nv=1000,radius=0.08)
+          
+        }
+        x_cor_vector2 = x.tip+2*strwidth("W")
+        for(i in 1:length(x.tip)){
+          x_cor = x_cor_vector2[i]
+          y_cor = y.tip[i]
+          if(phy_loc[,2][i] == 1) draw.circle(x_cor,y_cor,nv=1000,radius=0.08,col="purple")
+          else       draw.circle(x_cor,y_cor,nv=1000,radius=0.08)    }
+        
+      }
+}
