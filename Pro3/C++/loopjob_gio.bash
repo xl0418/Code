b01abffe9b4365093718bf275b6593d9cc9e6776
Liang Xu <xl0418@gmail.com>
2018-06-28 15:16:20 +0200

    file name fixed

--- Pro3/C++/loopjob_gio.bash
+++ Pro3/C++/loopjob_gio.bash
@@ -9,7 +9,7 @@ for((m = 0; m <= 4; m++))
 do
 echo "#!/bin/bash" > zMLjob$s$m
 echo "#SBATCH --time=05:59:00" >> zMLjob$s$m
-echo "./jc phi=${phi[s]} psi=${psi[m]} sA=5 sB=1 ticks=10e6 seed=1 file="test${phi[s]}${psi[m]}.m" " >> zMLjob$s$m
+echo "./jc phi=${phi[s]} psi=${psi[m]} sA=5 sB=1 ticks=10e6 seed=1 file="test$s$m.m" " >> zMLjob$s$m
 echo "rm zMLjob$s$m" >> zMLjob$s$m
 
 #sbatch --partition=regular --mem=12GB --job-name=ML$s --mail-type=FAIL,TIME_LIMIT --mail-user=glaudanno@gmail.com zMLjob$s
