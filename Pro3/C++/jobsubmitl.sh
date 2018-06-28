bd4f464385eb26b24314f07962f6667eb3b41ef3
Liang Xu <xl0418@gmail.com>
2018-06-28 15:15:23 +0200

    Cluster job submission in parallel
    loopjob_gio.bash is the parallelizing job submission script.

--- Pro3/C++/jobsubmitl.sh
+++ Pro3/C++/jobsubmitl.sh
@@ -2,8 +2,6 @@
 #SBATCH --time=05:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=jcsim
-#SBATCH --output=jcsim.log
 #SBATCH --mem=1g
-./jc phi=$phi psi=$psi sA=5 sB=1 ticks=10e7 seed=1 file="test1.m"
+./jc phi=$1 psi=$2 sA=5 sB=1 ticks=10e6 seed=1 file="test$3.m"
 
