1a11837bd818e06415dc7f8100eaaa6c251e8f23
Liang Xu <xl0418@gmail.com>
2018-08-07 16:57:36 +0200

    abcpp added

--- Pro3/C++/jobsubmitl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --time=05:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem=1g
-./jc phi=$1 psi=$2 sA=5 sB=1 ticks=10e6 seed=1 file="test$3.m"
-
--- Pro3/C++/loopjob.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --array=1-25
-INPUTFILE=parameterfile.txt
-# get n-th line from $INPUTFILE
-ARGS=$(cat $INPUTFILE | head -n $SLURM_ARRAY_TASK_ID | tail -n 1)
-jobsubmitl.sh $ARGS $SLURM_ARRAY_TASK_ID
