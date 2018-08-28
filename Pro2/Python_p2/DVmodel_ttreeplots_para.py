Plot bug fixed; Standard deviation added.

Pro2/Python_p2/DVmodel_ttreeplots_para.py
@@ -12,8 +12,8 @@ theta = 0  # optimum of natural selection
 r = 1  # growth rate
 delta_pop = .001  # Variance of random walk of population
 Vmax = 1
-scalor = 1000
-K=2000000000
+scalor = 10000
+K=1000000000
 nu=1/(100*K)
 gamma_vec = np.array([0,0.001,0.01,0.1,0.5,1])
 a_vec = gamma_vec
@@ -51,9 +51,9 @@ for index_g in range(len(gamma_vec)):
             trait_dr_tips = trait_RI_dr[evo_time, :][~np.isnan(trait_RI_dr[evo_time, :])]
             population_tips = population_RI_dr[evo_time, :][~np.isnan(population_RI_dr[evo_time, :])]
 
-            trait_RI_dr[np.where(trait_RI_dr == 0)[0], np.where(trait_RI_dr == 0)[1]] = None
+            # trait_RI_dr[np.where(trait_RI_dr == 0)[0], np.where(trait_RI_dr == 0)[1]] = None
 
-            population_RI_dr[np.where(population_RI_dr == 0)[0], np.where(population_RI_dr == 0)[1]] = None
+            # population_RI_dr[np.where(population_RI_dr == 0)[0], np.where(population_RI_dr == 0)[1]] = None
             num_lines = total_species
 
             x = np.arange(evo_time + 1)

Pro2/abcpp/abcpp/evo.py
@@ -10,7 +10,7 @@ from dvtraitsim_shared import DVTreeData, DVParam
 import dvtraitsim_cpp as dvcpp
 from scipy.stats import norm
 
-gamma = 0.001
+gamma = 0.01
 a = 0.5
 
 # argsort of 2-dimensional arrays is awkward
@@ -29,9 +29,9 @@ def normalized_norm(x, y):
 # dir_path = os.path.dirname(os.path.realpath(__file__))
 # files = dir_path + '/../tree_data/example1/'
 dir_path = 'c:/Liang/Code/Pro2/abcpp'
-files = dir_path + '/tree_data/example1/'
+files = dir_path + '/tree_data/example2/'
 
-td = DVTreeData(path=files, scalar=1000)
+td = DVTreeData(path=files, scalar=10000)
 
 prior = [0.5, 0.5, 0.5, 0.5]
 gamma_prior_mean = prior[0]
@@ -89,15 +89,16 @@ for g in range(generations):
         V = pop['V'][valid][i, j]
         Z = np.nan_to_num(Z)
         V = np.nan_to_num(V)
+        #GOF: Goodness of fit
         fitness[valid] += 1.0 - normalized_norm(Z, obsZ)
-        fitness[valid] += 1.0 - normalized_norm(V, obsV)
+        fitness[valid] += 1.0 - normalized_norm(np.sqrt(V), np.sqrt(obsV))
 
     # print something...
     q5 = np.argsort(fitness)[-population// 20]  # best 5%
     fit_index = np.where(fitness > fitness[q5])[0]
 
     print('Generation = %d  gamma = %f  a = %f  fitness = %f' % (g, np.mean(params[fit_index, 0]),
-                                                                 np.mean(params[fit_index, 1]), np.mean(fitness[q5])))
+                                                                 np.mean(params[fit_index, 1]), np.mean(fitness[fit_index])))
 
     weight_gamma = weight_gamma[fit_index]/sum(weight_gamma[fit_index])
     weight_a = weight_a[fit_index]/sum(weight_a[fit_index])
@@ -140,9 +141,9 @@ for g in range(generations):
     params[:, 1] = propose_a
 #
 # para_data = {'gamma': gamma_data, 'a': a_data}
-# # file='C:/Liang/Code/Pro2/Python_p2/abcpp/abcpp/smcdata/'
-# file = '/home/p274981/abcpp/abcpp/'
-# file2 = file + 'smc.npy'
+# file='C:/Liang/Code/Pro2/abcpp/abcpp/smcndata/'
+# # file = '/home/p274981/abcpp/abcpp/'
+# file2 = file + 'paraT11.npy'
 # np.save(file2,para_data)
 #
 # smc = np.load(file2).item()