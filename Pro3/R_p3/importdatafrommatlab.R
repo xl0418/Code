16b60ddd36d8380087bee18aff9cc284c49a1389
Liang Xu <xl0418@gmail.com>
2018-10-04 16:46:33 +0200

    Data processing Pro3

--- Pro3/R_p3/importdatafrommatlab.R
+++ Pro3/R_p3/importdatafrommatlab.R
@@ -1,12 +1,15 @@
+library(DDD)
+dir = 'C:/Liang/Code/Pro3/10_9simdata/'
 
-source('C:/Liang/Code/Pro3/C++/Standalone/x64/Release/events2.Rdata')
-source('C:/Liang/Code/Pro3/C++/Standalone/x64/Release/turnover1.Rdata')
+source(paste0(dir,'events00.Rdata'))
+source(paste0(dir,'turnover00.Rdata'))
+source('C:/Liang/Code/Pro3/R_p3/event2L.R', echo=TRUE)
 
 events = as.data.frame(t(events))
 colnames(events) = c('T','ns','x','y','sp','ancestor')
 turnover
 result = list(turnover = turnover, events = events)
 L = event2L(result, mode = 'Matlab')
-phy = DDD::L2phylo(L,dropextinct = T)
+phy = DDD::L2phylo(L,dropextinct = F)
 plot(phy)
 
