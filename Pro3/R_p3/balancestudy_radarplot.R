c8d9093483aa27a5c0c9411286c176b3419b41c7
Liang Xu <xl0418@gmail.com>
2018-12-03 15:48:46 +0100

    ggradar for Pro3 added.

---
+++ Pro3/R_p3/balancestudy_radarplot.R
@@ -0,0 +1,62 @@
+library(ape)
+library(phytools)
+library(plyr)
+library(apTreeshape)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(ggthemes)
+
+library(ggradar)
+suppressPackageStartupMessages(library(dplyr))
+library(scales)
+library(tibble)
+
+method = 'gamma'
+
+# Compute the colless values and gamma values for a given phylo tree.
+foo <- function(x, metric = "colless") {
+  if (metric == "colless") {
+    xx <- as.treeshape(x)  # convert to apTreeshape format
+    colless(xx, "yule")  # calculate colless' metric
+  } else if (metric == "gamma") {
+    gammaStat(x)
+  } else stop("metric should be one of colless or gamma")
+}
+
+
+colless_alldf = data.frame()
+count = 0
+for(i in c(1:5)){
+  for(j in c(1:5)){
+    count = count+1
+    numtrees <- 1  # lets simulate 1000 trees
+    trees <- pbtree(n = 50, nsim = numtrees, ape = F)  # simulate 500
+    
+    # Generate a 'multiphylo' class to contain all trees generated under the same paras
+    # tree1 = pbtree(n = 50,nsim = 1)
+    # tree2 = pbtree(n = 50, nsim = 1)
+    # treelist = list(tree1,tree2)
+    # class(treelist) = 'multiphylo'
+    
+    colless_df <- foo(trees, metric = 'colless')  # calculate metric for each tree
+    gamma_df = foo(trees, metric = 'gamma')
+    richness = trees$Nnode+1
+    
+    colless_df = cbind(colless_df,gamma_df,richness,i,j)
+    colless_alldf = rbind(colless_alldf,colless_df)
+    
+  }
+}
+
+colnames(colless_alldf) = c('Colless','Gamma','Richness','psi','phi')
+
+tdf = colless_alldf
+tdf %>%
+  mutate_all(funs(rescale))%>%
+  tail(5) -> tdf1
+
+tdf1 = cbind(c('1','2','3','4','5'),tdf1)
+colnames(tdf1) = c('group',colnames(tdf1)[-1])
+ggradar(tdf1,legend.text.size = 8,font.radar = "Times") 
+
