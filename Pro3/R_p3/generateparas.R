20490d7830931f8acfc537903fa29a2755c27de6
Liang Xu <xl0418@gmail.com>
2019-01-03 13:01:42 +0100

    Format changed

--- Pro3/R_p3/generateparas.R
+++ Pro3/R_p3/generateparas.R
@@ -4,17 +4,19 @@ L = 333
 v=0.0001
 sA=1000
 sB=1000
-ticks=1e8
+ticks=1000000000
 log=1e8
 count = 0
 append = TRUE
-for(i in c(4:6)){
+for(i in c(1:6)){
   for(j in c(1:6)){
     count = count +1
-    str = paste0('L=',L,' v=',v,' phi=',phi_vec[i], ' psi=',psi_vec[j],
-                 ' sA=',sA, ' sB=',sB, ' ticks=',ticks, ' log=',log,' continue=neutral.m', ' file=phi',i,'psi',j
-                 ,'.m')
-    filename<-"C:/Liang/Googlebox/Research/Project3/simdata_181220/spatialparahighP2.txt"
+    # str = paste0('L=',L,' v=',v,' phi=',phi_vec[i], ' psi=',psi_vec[j],
+    #              ' sA=',sA, ' sB=',sB, ' ticks=',ticks, ' log=',log,' continue=neutral.m', ' file=Lphi',i,'psi',j
+    #              ,'.m')
+    str = sprintf('L=%i v=%.4f phi=%.4f psi=%.4f sA=%.1f sB=%.1f ticks=%i log=%i continue=neutral.m file=Hphi%ipsi%i.m',
+                  L,v,phi_vec[i],psi_vec[j],sA,sB,ticks, log,i,j)
+    filename<-"C:/Liang/Googlebox/Research/Project3/simdata_190103/spatialparahigh.txt"
     if(count == 1){
       # write(str, file=filename,append=FALSE)
       cat(str,'\n', file=filename, append=FALSE, sep='')
