292223b07dcec7df974d73e1def5b3cff7f186e2
Liang Xu <xl0418@gmail.com>
2019-01-03 15:27:30 +0100

    3 scenarios set

--- Pro3/R_p3/generateparas.R
+++ Pro3/R_p3/generateparas.R
@@ -2,8 +2,24 @@ phi_vec = c(0,0.001,0.01,0.1,0.5,1)
 psi_vec = c(0,0.001,0.01,0.1,0.5,1)
 L = 333
 v=0.0001
-sA=1000
-sB=1000
+
+scenario = 0
+if(scenario == 0){
+  sA=0.1
+  sB=0.1
+  sce = "L"
+}else if(scenario == 1){
+  sA=10
+  sB=10
+  sce = "M"
+}else if(scenario == 2){
+  sA=1000
+  sB=1000
+  sce = "H"
+}else{
+  print("Pls provide scenario in (0,1,2)!")
+}
+
 ticks=1000000000
 log=1e8
 count = 0
@@ -14,8 +30,8 @@ for(i in c(1:6)){
     # str = paste0('L=',L,' v=',v,' phi=',phi_vec[i], ' psi=',psi_vec[j],
     #              ' sA=',sA, ' sB=',sB, ' ticks=',ticks, ' log=',log,' continue=neutral.m', ' file=Lphi',i,'psi',j
     #              ,'.m')
-    str = sprintf('L=%i v=%.4f phi=%.4f psi=%.4f sA=%.1f sB=%.1f ticks=%i log=%i continue=neutral.m file=Hphi%ipsi%i.m',
-                  L,v,phi_vec[i],psi_vec[j],sA,sB,ticks, log,i,j)
+    str = sprintf('L=%i v=%.4f phi=%.4f psi=%.4f sA=%.1f sB=%.1f ticks=%i log=%i continue=neutral.m file=%sphi%ipsi%i.m',
+                  L,v,phi_vec[i],psi_vec[j],sA,sB,ticks, log,sce,i,j)
     filename<-"C:/Liang/Googlebox/Research/Project3/simdata_190103/spatialparahigh.txt"
     if(count == 1){
       # write(str, file=filename,append=FALSE)
