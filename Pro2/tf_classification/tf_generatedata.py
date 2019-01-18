cc9f34b344317c69554727b01f4bfc155cab224c
Liang Xu <xl0418@gmail.com>
2019-01-18 16:50:08 +0100

    generate train data sets in parallel

--- Pro2/tf_classification/tf_generatedata.py
+++ Pro2/tf_classification/tf_generatedata.py
@@ -42,39 +42,49 @@ td = DVTreeData(path=files, scalar=10000)
 
 gamma = 0.001
 a_vec = [0.0,0.001,0.01,0.1,0.5,1]
+a_test_vec = [0.75]
 # a_labels = [i for i in range(len(a_vec))]
 Z_train = []
 label_train = []
 K=10e8
 nu=1/(100*K)
 # let's try to find a true simulation:
-datasize_batch = 1000
+datasize_batch = 2000
 
 for count in range(len(a_vec)):
     a = a_vec[count]
 
     obs_param = DVParam(gamma=gamma, a=a, K=K, nu=nu, r=1, theta=0, Vmax=1, inittrait=0, initpop=500,
                         initpop_sigma=10.0, break_on_mu=False)
-    print('try to find a completed true simulation with gamma =', obs_param[0], 'and a =', obs_param[1], 'and nu =', obs_param[3],'...')
-    for r in range(datasize_batch):
-        print(r)
-        obs = dvcpp.DVSim(td, obs_param)
-        if obs['sim_time'] == td.sim_evo_time:
-            s = np.argsort(obs['Z'])
-            obsZ = obs['Z'][s]
-            Z_train.append(list(obsZ))
-            label_train = np.concatenate((label_train,[count]))
-    if obs['sim_time'] < td.evo_time:
-        print('hopeless, does not compute.')
-        sys.exit(-1)
 
-Z_train_ndarray = np.array(Z_train)
+    params = np.tile(obs_param, (datasize_batch, 1))  # duplicate
+
+    print('Batch ', count,': try to generate ', datasize_batch,' simulations with gamma =', obs_param[0], 'and a =', obs_param[1], 'and nu =', obs_param[3],'...')
+
+    pop = dvcpp.DVSim(td, params)
+    valid = np.where(pop['sim_time'] == td.sim_evo_time)[0]
+    if len(valid) < 20:
+        print("WARNING:Valid simulations are too scarce!")
+    if valid.size > 0:
+        Z = pop['Z'][valid]
+        i, j = argsort2D(Z)
+        Z = Z[i, j]
+        # V = pop['V'][valid][i, j]
+        # Z = np.nan_to_num(Z)
+        label_train_batch = np.zeros(shape=(1,Z.shape[0]))
+        label_train_batch.fill(count)
+    label_train = np.concatenate((label_train,label_train_batch),axis=None)
+    if count == 0:
+        Z_train = Z
+    else:
+        Z_train = np.concatenate((Z_train,Z))
+
 
 #
-para_data = {'Z_train': Z_train_ndarray, 'Z_labels': label_train}
+para_data = {'Z_train': Z_train, 'Z_labels': label_train}
 file='C:/Liang/Code/Pro2/tf_classification/'
 # # file = '/home/p274981/abcpp/abcpp/'
-filename = file + 'tf_c1_batch2.npy'
+filename = file + 'tf_trait2.npy'
 np.save(filename,para_data)
 #
 # smc = np.load(filename).item()
