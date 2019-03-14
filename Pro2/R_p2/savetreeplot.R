51b1cc619689f74caf6c7494ceb39772c7c66696
Liang Xu <xl0418@gmail.com>
2019-03-14 14:14:51 +0100

    Update savetreeplot.R

--- Pro2/R_p2/savetreeplot.R
+++ Pro2/R_p2/savetreeplot.R
@@ -1,11 +1,11 @@
 library(DDD)
 dir = 'C:/Liang/Googlebox/Research/Project2/treesim_newexp/example'
 
-for(i in c(1:22)){
+for(i in c(1:14)){
   filename = paste0(dir,i,'/Treedata.Rdata')
   pngname = paste0(dir,i,'/testtree',i,'.png')
   load(filename)
-  phy = DDD::L2phylo(result$L,dropextinct = T)
+  phy = DDD::L2phylo(result$L,dropextinct = F)
   png(filename=pngname)
   plot(phy,show.tip.label=FALSE)
   # title(paste("Phylogenetic tree",i),line=-25)
