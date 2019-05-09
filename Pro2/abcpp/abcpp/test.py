d6f5de3b2827eba4ec01f0bf14da06a7c0c143ed
Liang Xu <xl0418@gmail.com>
2019-05-09 13:29:53 +0200

    Model without pop dynamics & with metabolism added

--- Pro2/abcpp/abcpp/test.py
+++ Pro2/abcpp/abcpp/test.py
@@ -1,25 +1,53 @@
-import os
-from time import perf_counter
+import os,sys
+sys.path.append('C:/Liang/abcpp_ms2/abcpp')
 import numpy as np
-from dvtraitsim_shared import DVTreeData, DVParam
-import dvtraitsim_cpp as dvcpp
+from dvtraitsim_shared import DVTreeData, DVParamLiang
+sys.path.append('C:/Liang/Code/Pro2/abcpp/abcpp/')
+from Dvtraitsim_without_population import DVSimLiang_nopop
+from Dvtraitsim_metabolism import DVSimMetabolism
 
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-files = dir_path + '/../tree_data/example1/'
+#full tree
+dir_path = 'c:/Liang/Googlebox/Research/Project2/BaleenWhales/'
 
+files = dir_path + 'treedata/'
 td = DVTreeData(path=files, scalar=1000)
-param = DVParam(gamma=0.001, a=1, K=10000, nu=0.0001, r=1, theta=0, Vmax=1, inittrait=0, initpop=500, split_stddev=0.2)
-params = np.tile(param, (1000, 1))       # duplicate
 
-print('running cvtraits_cpp.DVSim on: ', files)
-start = perf_counter()
-dispatch = dvcpp.DVSim(td, param)
-print("in ", perf_counter() - start, "s")
+K = 1e6
+nu = 1e-4
+meantrait = 1300
 
-fitnes = np.random.uniform(1, 1000, 1000)
-start = perf_counter()
-dvcpp.discrete_distribution(fitnes, 1000)
-print("in ", perf_counter() - start, "s")
+obs_param = DVParamLiang(gamma=1e-7, a=1e-4, K=K,h=1, nu=nu, r=1, theta=meantrait,V00=.1,V01=.1, Vmax=100, inittrait=meantrait, initpop=1e5,
+                                initpop_sigma=10.0, break_on_mu=False)
+
+for replicate in range(100):
+    print(replicate)
+
+    simresult = DVSimLiang_nopop(td, obs_param)
+
+    if simresult['sim_time'] == td.sim_evo_time:
+        pic = 0
+        break
+    else:
+        pic = 1
+
+
+K = 1e13
+nu = 1e-4
+meantrait = 1300
+
+param_metabolism = DVParamLiang(gamma=1e-6, a=1e-4, K=K,h=1, nu=nu, r=1, theta=meantrait,V00=.1,V01=.1, Vmax=100, inittrait=meantrait, initpop=1e7,
+                                initpop_sigma=10.0, break_on_mu=False)
+
+for replicate in range(100):
+    print(replicate)
+
+    simresult = DVSimMetabolism(td, param_metabolism)
+
+    if simresult['sim_time'] == td.sim_evo_time:
+        pic = 0
+        break
+    else:
+        pic = 1
 
 
