fda91ee13296a2ba831abb4d01edddbc7f3484d0
Liang Xu <xl0418@gmail.com>
2019-10-22 17:38:51 +0200

    Bug fixed

--- Pro3/R_p3/event2L.R
+++ Pro3/R_p3/event2L.R
@@ -1,5 +1,5 @@
 
-event2L = function(events,turnover){
+# event2L = function(events,turnover){
   colnames(events) = c('T','ns','x','y','sp','ancestor')
   ticks = turnover
   L_raw = cbind(events$T,events$ancestor+1,events$sp+1)
@@ -10,25 +10,45 @@ event2L = function(events,turnover){
   L_raw[,1] = ticks - L_raw[,1]
   
   ext_rownum = which(L_raw[,2] ==-1)
+  ext.spec.index = L_raw[ext_rownum,3]
+  ext.time.vec = L_raw[ext_rownum,1]
   if(length(ext_rownum)>0){
     
-    for(ext.row in ext_rownum){
-      L_raw[ext.row,4] = 0
-      ext.spec = L_raw[ext.row,3]
-      ext.spec.time = L_raw[ext.row,1]
+    for(indicator in c(1:length(ext_rownum))){
 
+      corrector = 2*(indicator-1)
+      ext.spec = ext.spec.index[indicator]
+      ext.row = ext_rownum[indicator]-corrector
+      ext.spec.time = ext.time.vec[indicator]
       dead.spec.row.candidate = which(L_raw[1:(ext.row-1),3]==ext.spec)
-      dead.spec.row = dead.spec.row.candidate[which(L_raw[dead.spec.row.candidate,4]==-1)]
-      if(length(dead.spec.row)!=1){
+
+      if(length(dead.spec.row.candidate)!=1){
         print("More species are dieing than expected (1).")
         break
       }
-
-      L_raw[dead.spec.row,4]=L_raw[ext.row,1]
+      possible.ext.mother = which(L_raw[1:(ext.row-1),2]==ext.spec)
+      if(length(possible.ext.mother)>0){
+        mother.mother = L_raw[ext.spec,2]
+        L_raw[possible.ext.mother,2] = mother.mother
+        
+      }
+      
       change.spec = which(L_raw[1:(ext.row-1),3]>ext.spec)
       L_raw[change.spec,3] = L_raw[change.spec,3]-1
       change.mot.spec = which(L_raw[1:(ext.row-1),2]>ext.spec)
       L_raw[change.mot.spec,2] = L_raw[change.mot.spec,2]-1
+      if(length(which(L_raw[change.mot.spec,1]==176726339))){
+        print("The target branching time is changing")
+      }
+      
+      
+      # remove the extinction information row
+      L_raw = L_raw[-ext.row,]
+      # remove the extinct species
+      L_raw = L_raw[-dead.spec.row.candidate,]
+      if(length(which(L_raw[,2]==L_raw[,3]))>=1){
+        print("Born from itself!")
+      }
       
       
       # 
@@ -42,5 +62,5 @@ event2L = function(events,turnover){
     L <- L_ini[which(L_ini[,4]==-1),]
   }
 
-  return(L)
-}
+  # return(L)
+# }
